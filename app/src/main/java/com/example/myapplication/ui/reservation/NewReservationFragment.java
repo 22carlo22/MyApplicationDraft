e3df3f682ee9450b09eb25697b274a7ab3fe0f9c
Stanley Tan <stanleytan2002@live.ca>
2024-11-10 23:41:49 -0500

    update - activityfile + fragments + name changes of files for RESERVATION

--- app/src/main/java/com/example/myapplication/ui/reservation/NewReservationFragment.java
+++ app/src/main/java/com/example/myapplication/ui/reservation/NewReservationFragment.java
@@ -1,22 +1,14 @@
 package com.example.myapplication.ui.reservation;
 
-import android.content.Intent;
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.Toast;
 
-import com.example.myapplication.R;
 import com.example.myapplication.databinding.FragmentNewReservationBinding;
-import com.example.myapplication.ui.boards.MyBoardActivity;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class NewReservationFragment extends Fragment {
 
@@ -26,27 +18,16 @@ public class NewReservationFragment extends Fragment {
         // Required empty public constructor
     }
 
-    View view;
-    Button newReservation;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstancedState){
-        //return inflater.inflate(R.layout.fragment_new_reservation,parent,false);
-        view = inflater.inflate(R.layout.fragment_new_reservation,parent,false);
-        newReservation = (Button) view.findViewById(R.id.testbutton);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
 
-        newReservation.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                Toast.makeText(getActivity(),"New Reservation", Toast.LENGTH_LONG).show();
-            }
-        });
-        return view;
-    }
+        //Extract the views from this fragment and display it
+        binding = FragmentNewReservationBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
 
-    @Override
-    public void onViewCreated(View view, Bundle savedInstancedState){
-
+        return root;
     }
 
     @Override
